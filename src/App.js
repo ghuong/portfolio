4c1b5a5a61f5be83d34b9210d74e3e9216b9fdce
Gary Huong <garyhuong@gmail.com>
2022-05-10 11:24:43 -0400

    Education

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
+import Education from "./components/Education";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <About />
       <Projects />
       <Skills />
+      <Education />
       <Contact />
     </main>
   );
--- src/data.js
+++ src/data.js
@@ -10,8 +10,7 @@ export const projects = [
   {
     title: "Real-Time Group Chat",
     subtitle: "Node, React Hooks, Socket.io",
-    description:
-      "Chatrooms with real-time chat updates using Socket.io.",
+    description: "Chatrooms with real-time chat updates using Socket.io.",
     image: "./images/chat.png",
     link: "https://group-chat-gary.herokuapp.com",
   },
@@ -26,9 +25,8 @@ export const projects = [
   {
     title: "Project 4",
     subtitle: "Technologies",
-    description:
-      "Description",
-    image: "./images/mutex.png",
+    description: "Description",
+    image: "./images/project4.png",
     link: "https://mutex-gary.netlify.app",
   },
 ];
@@ -41,7 +39,7 @@ export const skills = [
   "SQL",
   "MongoDB",
   "Ruby on Rails",
-  "Android Apps"
+  "Android Apps",
 ];
 
 export const contact = {
@@ -51,9 +49,27 @@ export const contact = {
   // from embed-map.com:
   map: "https://www.google.com/maps/embed/v1/place?q=north+york&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8",
   emailSubject: "Professional inquiry from garyhuong.netlify.app",
-  text: "Interested in working with me? You can reach me here, or send me an email directly at garyhuong@gmail.com."
+  text: "Interested in working with me? You can reach me here, or send me an email directly at garyhuong@gmail.com.",
 };
 
 export const socialmedia = {
-  github: "https://github.com/ghuong"
-}
+  github: "https://github.com/ghuong",
+};
+
+export const education = {
+  schools: [
+    {
+      name: "University of Waterloo",
+      degree: "Bachelor of Computer Science",
+    },
+    {
+      name: "University of Helsinki",
+      degree: "Full Stack 2022 Certificate",
+    },
+    {
+      name: "Wilfrid Laurier University",
+      degree: "Bachelor of Business Administration",
+    },
+  ],
+  text: "I am a Computer Science graduate from the University of Waterloo. Some of my notable courses are: Networks, Security, User Interfaces, A.I., Distributed Systems, Concurrency, Graphics, Operating Systems. I also did University of Helsinki's full-stack 2022 bootcamp, learning Node, React, and MongoDB.",
+};
