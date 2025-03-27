71b55f0a90eb68fad5d39f09de0f3a087ed30e90
Gary Huong <garyhuong@gmail.com>
2025-03-27 16:33:24 -0400

    social media links

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function About() {
   return (
-    <section id="about" className="relative h-[calc(100vh-5rem)]">
+    <section id="about" className="relative h-screen -translate-y-20">
       <div className="md:px-20 h-full">
         <div className="container mx-auto h-full flex flex-col md:flex-row-reverse items-center justify-center">
           <div className="w-1/2 lg:w-2/5 lg:max-w-lg">
@@ -15,14 +15,14 @@ export default function About() {
           </div>
           <div className="md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left items-center text-center">
             <div className="mb-8">
-              <div className="sm:text-6xl text-4xl font-abrilFatface mb-5">I Am</div>
+              <div className="sm:text-6xl text-4xl font-abrilFatface mb-5">
+                I Am
+              </div>
               <div className="sm:text-[2.75rem] text-3xl font-abrilFatface">
                 Software <span className="text-dark-blue">Developer</span>
               </div>
             </div>
-            <p className="mb-8 leading-relaxed font-poppins">
-              {about.text}
-            </p>
+            <p className="mb-8 leading-relaxed font-poppins">{about.text}</p>
             <div className="flex justify-center gap-5">
               <button className="flex items-center gap-2 rounded-full bg-color-main shadow-main text-white px-5 py-3 font-poppins">
                 <i className="bx bx-file"></i>
@@ -44,28 +44,26 @@ export default function About() {
                 See What I've Built
               </a> */}
             </div>
-            <div className="flex flex-row w-full mt-8">
+            <div className="absolute flex flex-row gap-12 bottom-20 text-2xl">
               <a
                 href={socialmedia.github}
                 target="_blank"
                 rel="noreferrer noopener"
               >
-                <img
-                  src="./images/github.png"
-                  alt="github-logo"
-                  className="w-7"
-                />
+                <i className="bx bxl-github transition-colors duration-300 hover:text-color-main" />
+              </a>
+              <a href="#about">
+                <i className="bx bxl-dribbble transition-colors duration-300 hover:text-color-main" />
               </a>
               <a
                 href={socialmedia.linkedin}
                 target="_blank"
                 rel="noreferrer noopener"
               >
-                <img
-                  src="./images/linkedin.png"
-                  alt="linkedin-logo"
-                  className="w-7 ml-4"
-                />
+                <i className="bx bxl-linkedin-square transition-colors duration-300 hover:text-color-main" />
+              </a>
+              <a href="#contact">
+                <i className="bx bxs-envelope transition-colors duration-300 hover:text-color-main" />
               </a>
             </div>
           </div>
