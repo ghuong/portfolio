bbb663a7b1e4c6ecbb20586c042c3e0ee43fec1e
Gary Huong <garyhuong@gmail.com>
2025-03-27 16:47:06 -0400

    minor change

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <div className="absolute w-full h-full bg-transparent blur-2xl -z-10">
         <div className="absolute rounded-full -z-50 bg-[] bg-color-second w-44 h-44"></div>
         <div className="absolute rounded-full -z-50 bg-color-second w-24 h-24 top-40 right-28"></div>
-        <div className="absolute rounded-full -z-50 bg-color-main w-12 h-12 bottom-20 left-[60%]"></div>
+        <div className="absolute rounded-full -z-50 bg-color-main w-12 h-12 bottom-24 left-[60%]"></div>
       </div>
       <Navbar />
       <About />
