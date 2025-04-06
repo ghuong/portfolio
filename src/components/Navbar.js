c153799652b3c34c9d3eb6df95415692a6a0f579
Gary Huong <garyhuong@gmail.com>
2025-04-06 16:12:36 -0400

    responsive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,18 +3,22 @@ import Navlink from "./Navlink";
 
 export default function Navbar() {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   useEffect(() => {
     const isScrolled = () => {
       const threshold = 100;
       return window.scrollY > threshold;
-    }
+    };
 
     const isScrolledToBottom = () => {
       const threshold = 10;
       // viewport height plus scrolled height equals document's height
-      return (window.innerHeight + window.scrollY) >= (document.body.offsetHeight - threshold);
-    }
+      return (
+        window.innerHeight + window.scrollY >=
+        document.body.offsetHeight - threshold
+      );
+    };
 
     const handleScroll = () => {
       if (isScrolled() && !isScrolledToBottom()) {
@@ -31,9 +35,13 @@ export default function Navbar() {
     };
   }, []);
 
+  const handleHamburgerMenuClicked = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header
-      className={`md:sticky top-0 z-50 h-20 font-abrilFatface
+      className={`md:sticky top-0 z-40 h-20 font-abrilFatface
         transition duration-1000 transform
         ${
           isScrolled
@@ -42,7 +50,7 @@ export default function Navbar() {
         }`}
     >
       <div
-        className={`flex flex-wrap h-full px-24 flex-col md:flex-row items-center justify-between
+        className={`relative flex flex-wrap h-full px-12 lg:px-24 flex-col md:flex-row items-center justify-between
           transition-transform transform duration-1000 
             ${isScrolled ? "translate-y-2" : "translate-y-0"}
           `}
@@ -50,7 +58,18 @@ export default function Navbar() {
         <a href="#about" className="text-4xl">
           Gary Huong.
         </a>
-        <div className="flex">
+        <div
+          className={`
+          flex items-center z-50
+          absolute flex-col top-20 right-0 bg-[rgba(255,255,255,0.8)] w-[19rem] py-14 gap-8
+          rounded-[1.25rem_0_1.25rem_1.25rem] shadow-[-8px_8px_20px_rgba(0,0,0,0.2)] backdrop-blur-lg
+          lg:relative lg:flex-row lg:top-0 lg:bg-transparent lg:w-auto lg:py-0 lg:gap-12
+          lg:border-none lg:shadow-none lg:backdrop-blur-none
+          xl:gap-14
+          transition-transform transform duration-1000
+          ${isScrolled ? "-translate-y-2 lg:translate-y-0" : "translate-y-0"}
+          ${isMenuOpen ? "flex" : "hidden lg:flex"}`}
+        >
           <Navlink text="Work" href={"#work"} />
           <Navlink text="Projects" href={"#projects"} />
           <Navlink text="Education" href={"#education"} />
@@ -65,8 +84,11 @@ export default function Navbar() {
           >
             Contact
           </a>
-          <i className="bx bx-moon text-lg pl-6"></i>
-          <i className="bx bx-menu text-2xl pl-6 hidden"></i>
+          <i className="bx bx-moon cursor-pointer hover:text-color-main transition-colors duration-300 text-lg pl-6"></i>
+          <i
+            className="bx bx-menu cursor-pointer hover:text-color-main transition-colors duration-300 text-2xl pl-6 lg:hidden"
+            onClick={handleHamburgerMenuClicked}
+          ></i>
         </div>
       </div>
     </header>
--- src/components/Navlink.js
+++ src/components/Navlink.js
@@ -4,7 +4,7 @@ export default function Navlink({ text, href }) {
   return (
     <a
       href={href}
-      className="relative text-lg mx-7 hover:text-color-main transition-all duration-300
+      className="relative text-lg hover:text-color-main transition-all duration-300
         after:absolute after:left-0 after:-bottom-2 after:w-full after:h-[2px] after:bg-color-main 
         after:scale-x-0 hover:after:scale-x-100
         after:transition-transform after:duration-300 
