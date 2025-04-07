44b3b1c0aec0d78a4c005707f0468257a8259188
Gary Huong <garyhuong@gmail.com>
2025-04-06 20:40:55 -0400

    blur change

--- src/App.js
+++ src/App.js
@@ -19,11 +19,11 @@ function App() {
     <main className={`${isDarkMode ? "dark" : ""} relative`}>
       <div className="absolute w-full h-screen -z-20">
         <div className="absolute rounded-full bg-color-second dark:bg-darkmode-color-second transition-colors duration-300 w-44 h-44"></div>
-        <div className="absolute rounded-full bg-color-second dark:bg-darkmode-color-second transition-colors duration-300 w-28 h-28 top-40 right-28"></div>
+        <div className="absolute rounded-full bg-color-second dark:bg-darkmode-color-second transition-colors duration-300 w-28 h-28 top-36 right-28"></div>
         <div className="absolute rounded-full bg-color-main w-12 h-12 bottom-24 left-[60%]"></div>
       </div>
       <div className="absolute w-full h-full -z-50 bg-transparent dark:bg-[#222] transition-colors duration-300"></div>
-      <div className="absolute w-full h-full bg-transparent -z-10 backdrop-blur-2xl"></div>
+      <div className="absolute w-full h-full bg-transparent -z-10 backdrop-blur-2xl dark:backdrop-blur-[50px]"></div>
       <Navbar onToggleDarkMode={toggleIsDarkMode} />
       <About />
       <Work />
