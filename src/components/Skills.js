0fe3435d75f167ccdb783ac59ccb4ea9a4e7c974
Gary Huong <garyhuong@gmail.com>
2025-04-14 13:03:54 -0400

    Skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,7 +6,7 @@ export default function Skills() {
   return (
     <section id="skills">
       <div className="container px-5 pb-10 pt-28 h-screen mx-auto">
-        <div className="text-center mb-14">
+        <div className="text-center mb-10">
           <ChipIcon className="w-10 inline-block mb-4 text-dark-blue dark:text-darkmode-dark-blue" />
           <h1 className="text-4xl font-abrilFatface dark:text-white mb-4">
             Skills &amp; Technologies
@@ -20,9 +20,9 @@ export default function Skills() {
         <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
           {skills.map((skill) => (
             <div key={skill} className="p-2 sm:w-1/2 w-full">
-              <div className="bg-gray-800 rounded flex p-4 h-full items-center">
-                <BadgeCheckIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
-                <span className="title-font font-medium text-white">
+              <div className="bg-stone-100 dark:bg-darkmode-color-second rounded flex p-4 h-full items-center">
+                <BadgeCheckIcon className="text-green-500 dark:text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
+                <span className="font-poppins dark:text-white">
                   {skill}
                 </span>
               </div>
